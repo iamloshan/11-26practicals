64cded9beebbe8277d21c2811ed6a68dc65b2f5e
Logambigan Thiruganeshapathy <logambiganthiruganeshapathy@gmail.com>
2024-12-03 20:47:34 +0530

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './assets/CSS/layout.css';
-import Products from './components/Products';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="main-grid-container">
-      <Products/>
+    <div className='App'>
+      
+        {/* <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav> */}
+          {/* <Routes>
+            <Route path='/home' element={<Home/>} />
+            <Route path='/about' element={<AboutUs/>} />
+            <Route path='/contact' element={<Contactus/>} />
+            
+            </Routes> */}
+
+   <Router>
+            <Routes>
+            <Route path='/' element={<Login/>} />
+            <Route path='/dashboard' element={<Dashboard/>} /> 
+            </Routes> 
+      </Router>
+
+
     </div>
   );
 }
